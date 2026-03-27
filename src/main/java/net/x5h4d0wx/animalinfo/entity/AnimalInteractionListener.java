ec839c87709c4e994b645a6fae5369e1253922db
sbennett99 <seth81599@hotmail.com>
2026-03-27 01:34:09 -0500

    version bump and MojMap

--- src/main/java/net/x5h4d0wx/animalinfo/entity/AnimalInteractionListener.java
+++ src/main/java/net/x5h4d0wx/animalinfo/entity/AnimalInteractionListener.java
@@ -2,19 +2,20 @@ package net.x5h4d0wx.animalinfo.entity;
 
 
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.passive.GoatEntity;
-import net.minecraft.entity.passive.PandaEntity;
-import net.minecraft.entity.passive.AbstractHorseEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.MutableText;
-import net.minecraft.text.Text;
-import net.minecraft.text.Texts;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.EntityHitResult;
-import net.minecraft.world.World;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.animal.equine.AbstractHorse;
+import net.minecraft.world.entity.animal.goat.Goat;
+import net.minecraft.world.entity.animal.panda.Panda;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.level.Level;
+
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.ComponentUtils;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
@@ -38,51 +39,51 @@ public class AnimalInteractionListener implements UseEntityCallback {
         executor.setMaximumPoolSize(2);
     }
     @Override
-    public ActionResult interact(PlayerEntity player, World world, Hand hand, Entity entity, @Nullable EntityHitResult hitResult) {
-        Text outText = null;
+    public InteractionResult interact(Player player, Level world, InteractionHand hand, Entity entity, @Nullable EntityHitResult hitResult) {
+        Component outText = null;
         // CommandDispatcher<FabricClientCommandSource> test = ClientCommandManager.getActiveDispatcher();
         // player.sendMessage(Text.of(String.valueOf(test==null)), true);
         exec_scheduled.set(false);
 
-        if (!player.hasVehicle() && !player.isSneaking()){
-            return ActionResult.PASS;
+        if (!player.isPassenger() && !player.isCrouching()){
+            return InteractionResult.PASS;
         }
 
         int id = entity.getId();
         if (cooldownEntity.containsKey(id) && System.currentTimeMillis() < cooldownEntity.get(id)){
-            return ActionResult.PASS;
+            return InteractionResult.PASS;
         }
         cooldownEntity.put(id, System.currentTimeMillis() + 200);
 
         switch (entity) {
-            case PandaEntity panda:
+            case Panda panda:
                 // From the original Panda Info
-                MutableText mainGene = Text.translatable("gui.panda-info.gene." + panda.getMainGene());
-                MutableText hiddenGene = Text.translatable("gui.panda-info.gene." + panda.getHiddenGene());
-                MutableText productGene = Text.translatable("gui.panda-info.gene." + panda.getProductGene());
+                MutableComponent mainGene = Component.translatable("gui.panda-info.gene." + panda.getMainGene().getSerializedName());
+                MutableComponent hiddenGene = Component.translatable("gui.panda-info.gene." + panda.getHiddenGene().getSerializedName());
+                MutableComponent productGene = Component.translatable("gui.panda-info.gene." + panda.getVariant().getSerializedName());
 
-                mainGene = Text.translatable("gui.panda-info.main_gene", "§6", mainGene);
-                hiddenGene = Text.translatable("gui.panda-info.hidden_gene", "§b", hiddenGene);
-                productGene = Text.translatable("gui.panda-info.product_gene", "§a", productGene);
+                mainGene = Component.translatable("gui.panda-info.main_gene", "§6", mainGene);
+                hiddenGene = Component.translatable("gui.panda-info.hidden_gene", "§b", hiddenGene);
+                productGene = Component.translatable("gui.panda-info.product_gene", "§a", productGene);
 
-                outText = Texts.join(List.of(mainGene, hiddenGene, productGene), Text.of(" §r| "));
-
-                break;
-
-            case AbstractHorseEntity horsish:
-                final double blockSpeed = horsish.getAttributeValue(EntityAttributes.MOVEMENT_SPEED)* HORSE_SPEED_CONVERSION;
-                final double blockJump = jumpStrToBlocks(horsish.getAttributeValue(EntityAttributes.JUMP_STRENGTH));
-                final double health = horsish.getAttributeValue(EntityAttributes.MAX_HEALTH);
-                outText = Text.of(String.format("§6Speed: %1$.2f §r| §bJump %2$.2f §r| §aHearts %3$.2f ",blockSpeed, blockJump, health/2));
+                outText = ComponentUtils.formatList(List.of(mainGene, hiddenGene, productGene), Component.literal(" §r| "));
 
                 break;
 
-            case GoatEntity goat:
-                if (goat.isScreaming()){
-                    outText = Text.of("§bScreaming Goat");
+            case AbstractHorse horsish:
+                final double blockSpeed = horsish.getAttributeValue(Attributes.MOVEMENT_SPEED)* HORSE_SPEED_CONVERSION;
+                final double blockJump = jumpStrToBlocks(horsish.getAttributeValue(Attributes.JUMP_STRENGTH));
+                final double health = horsish.getAttributeValue(Attributes.MAX_HEALTH);
+                outText = Component.literal(String.format("§6Speed: %1$.2f §r| §bJump %2$.2f §r| §aHearts %3$.2f ",blockSpeed, blockJump, health/2));
+
+                break;
+
+            case Goat goat:
+                if (goat.isScreamingGoat()){
+                    outText = Component.literal("§bScreaming Goat");
                 }
                 else {
-                    outText = Text.of("Normal Goat");
+                    outText = Component.literal("Normal Goat");
                 }
 
                 break;
@@ -95,27 +96,27 @@ public class AnimalInteractionListener implements UseEntityCallback {
             sendLingeringMessage(player, entity, outText);
         }
 
-        return ActionResult.PASS;
+        return InteractionResult.PASS;
     }
 
     private static double jumpStrToBlocks(double jumpPower){
         return (JUMP_A * jumpPower * jumpPower) + (JUMP_B * jumpPower);
     }
 
-    private static void lingeringMessageHandeler(PlayerEntity player, Entity entity ,Text message){
-        player.sendMessage(message, true);
-        if(player.canInteractWithEntity(entity, 0) && exec_scheduled.get()){
+    private static void lingeringMessageHandeler(Player player, Entity entity ,Component message){
+        player.sendOverlayMessage(message);
+        if(player.isWithinEntityInteractionRange(entity,0) && exec_scheduled.get()){
             executor.schedule(() -> lingeringMessageHandeler(player, entity, message), 100, TimeUnit.MILLISECONDS);
         }else{
             exec_ended.set(true);
         }
     }
 
-    private static void sendLingeringMessage(PlayerEntity player, Entity entity ,Text message){
+    private static void sendLingeringMessage(Player player, Entity entity ,Component message){
         final String messageCopy = message.getString();
         while (!exec_ended.get()) {}
         exec_scheduled.set(true);
         exec_ended.set(false);
-        lingeringMessageHandeler(player, entity, Text.of(messageCopy));
+        lingeringMessageHandeler(player, entity, Component.literal(messageCopy));
     }
 }
